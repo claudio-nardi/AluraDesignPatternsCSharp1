c5dd460d45ecfba23be9de1cb6b30431dee777df
Claudio Nardi <p22.nardi@webmotors.com.br>
2022-06-02 05:04:18 -0300

    Add project files.

---
+++ Aula01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula01
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var orcamento = new Orcamento(5000);
+			var icms = new Icms();
+			var iss = new Iss();
+
+			Console.WriteLine($"Orcamento: {orcamento.Valor}");
+			Console.WriteLine($"ICMS: {icms.Calcula(orcamento)}");
+			Console.WriteLine($"ISS: {iss.Calcula(orcamento)}");
+
+		}
+	}
+}
